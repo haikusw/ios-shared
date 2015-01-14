f1ae3140bfea6f685128a35861eff417a20bf01b
Ben Ford <bentford@gmail.com>
2015-01-14 14:52:33 -0800

    Fixed bug in iOS8 where `UINavigationDelegate willShowViewController` isn't called during navigation.

--- Source/UI/SDPullNavigationManager.m
+++ Source/UI/SDPullNavigationManager.m
@@ -126,12 +126,20 @@
 {
     // Dismiss any modals that might be currently visible.
     UINavigationController* selectedNavController = (UINavigationController*)self.globalPullNavController.selectedViewController;
-    if(selectedNavController.visibleViewController.presentingViewController)
-        [selectedNavController.visibleViewController.presentingViewController dismissViewControllerAnimated:NO completion:nil];
+    if(selectedNavController.visibleViewController.presentingViewController) {
+        [selectedNavController.visibleViewController.presentingViewController dismissViewControllerAnimated:NO completion:^{
+            // Now navigate.
+            if([self navigateToTopLevelController:topLevelViewControllerClass])
+                [(UINavigationController*)self.globalPullNavController.selectedViewController popToRootViewControllerAnimated:animate];
 
-    // Now navigate.
-    if([self navigateToTopLevelController:topLevelViewControllerClass])
-        [(UINavigationController*)self.globalPullNavController.selectedViewController popToRootViewControllerAnimated:animate];
+        }];
+    }
+    else {
+        // Now navigate.
+        if([self navigateToTopLevelController:topLevelViewControllerClass])
+            [(UINavigationController*)self.globalPullNavController.selectedViewController popToRootViewControllerAnimated:animate];
+    }
+
 }
 
 #pragma mark - Navigation Automation
