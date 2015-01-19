74316b329d0dae175f88cffc2b4fdf0cbd9ae5aa
Bob Spryn <bob@thumbworks.io>
2015-01-19 15:39:40 -0500

    Cleaning up the NSNumber SDExtension

--- Source/Foundation/NSNumber+SDExtensions.m
+++ Source/Foundation/NSNumber+SDExtensions.m
@@ -8,63 +8,52 @@
 
 #import "NSNumber+SDExtensions.h"
 
+
 @implementation NSNumber (SDExtensions)
 
-+ (NSNumber *)numberFromDollarString:(NSString *)argDollarString
-{
-    static NSNumberFormatter *sFormatter = nil;
-    static dispatch_once_t onceToken;
-    
-    dispatch_once(&onceToken, ^{
-        sFormatter = [[NSNumberFormatter alloc] init];
-        [sFormatter setLocale:[NSLocale localeWithLocaleIdentifier:@"en_US"]];
-        [sFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
-    });
-    
-    return [sFormatter numberFromString:argDollarString];
-}
-
-/**
- Note that this is currently set to locale en_US but should be set to the correct locale
- once it needs to be localized to other regions and languages.
- */
-+ (NSString *)dollarStringFromNumber:(NSNumber *)argNumber
-{
++ (NSNumberFormatter *) commonCurrencyFormatter {
     static NSNumberFormatter *currencyFormatter = nil;
     static dispatch_once_t onceToken;
-    
+    /**
+     Note that this is currently set to locale en_US but should be set to the correct locale
+     once it needs to be localized to other regions and languages.
+     */
     dispatch_once(&onceToken, ^{
         currencyFormatter = [[NSNumberFormatter alloc] init];
         [currencyFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
         [currencyFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
     });
+    return currencyFormatter;
+}
+
++ (NSNumberFormatter *) commonDecimalFormatter {
+    static NSNumberFormatter *numberFormatter = nil;
+    static dispatch_once_t onceToken;
     
-    return [currencyFormatter stringFromNumber:argNumber];
+    dispatch_once(&onceToken, ^{
+        numberFormatter = [[NSNumberFormatter alloc] init];
+        [numberFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
+    });
+    return numberFormatter;
+}
+
++ (NSNumber *)numberFromDollarString:(NSString *)argDollarString
+{
+    return [[self commonCurrencyFormatter] numberFromString:argDollarString];
+}
+
++ (NSString *)dollarStringFromNumber:(NSNumber *)argNumber
+{
+    return [[self commonCurrencyFormatter] stringFromNumber:argNumber];
 }
 
 + (NSNumber *)numberFromString:(NSString *)argString
 {
-    static NSNumberFormatter *numberFormatter = nil;
-    static dispatch_once_t onceToken;
-
-    dispatch_once(&onceToken, ^{
-        numberFormatter = [[NSNumberFormatter alloc] init];
-        [numberFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
-    });
-    
-    return [numberFormatter numberFromString:argString];
+    return [[self commonDecimalFormatter] numberFromString:argString];
 }
 
 + (NSString *)stringFromNumber:(NSNumber *)argNumber
 {
-    static NSNumberFormatter *numberFormatter = nil;
-    static dispatch_once_t onceToken;
-    
-    dispatch_once(&onceToken, ^{
-        numberFormatter = [[NSNumberFormatter alloc] init];
-        [numberFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
-    });
-    
-    return [numberFormatter stringFromNumber:argNumber];
+    return [[self commonDecimalFormatter] stringFromNumber:argNumber];
 }
 @end
